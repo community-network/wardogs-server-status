fcfdcd2c9d0926d543594e9261bcf75f8800c78b
iiTzArcur <zefanjaj@gmail.com>
2026-09-18 22:24:21 +0200

    update workflow

---
+++ start.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+SET token=DISCORDTOKEN
+SET ip=127.0.0.1
+SET port=9000
+SET password=PASS
+
+wardogs_server_status.exe
---
+++ start.sh
@@ -0,0 +1,5 @@
+export token=TOKEN
+export ip=127.0.0.1
+export port=9000
+export password=PASS
+./wardogs_server_status
